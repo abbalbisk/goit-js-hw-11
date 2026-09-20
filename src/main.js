013c5e459492b753e8f3a9dbdbfa1faadc0a6748
Vova <Vova.gorlovoy@gmail.com>
2026-09-20 15:34:39 +0300

    updates

--- src/main.js
+++ src/main.js
@@ -1,23 +1,15 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
-import axios from "axios";
 import { getImagesByQuery } from './js/pixabay-api';
-import {createGallery} from './js/render-functions'
-import { clearGallery } from './js/render-functions';
-import {showLoader} from './js/render-functions'
-import {hideLoader} from './js/render-functions'
-
-//======================================
+import { createGallery, clearGallery, showLoader, hideLoader } from './js/render-functions';
 
 const formEl = document.querySelector('.form');
 const inputValue = document.querySelector('[type="text"]');
-const gallery = document.querySelector('.gallery');
-
 
 formEl.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    let inpValue = inputValue.value.trim();
+    const inpValue = inputValue.value.trim();
 
     if (!inpValue) {
         iziToast.error({
@@ -26,8 +18,10 @@ formEl.addEventListener("submit", (e) => {
         clearGallery();
         return;
     }
-     showLoader();
 
+
+    clearGallery();
+    showLoader();
 
     getImagesByQuery(inpValue)
         .then(data => {
@@ -37,20 +31,19 @@ formEl.addEventListener("submit", (e) => {
                 iziToast.error({
                     message: 'Sorry, there are no images matching your search query. Please try again!'
                 });
-                clearGallery();
                 return;
             }
-            clearGallery();
-            createGallery(arrOfImage);
-            }).catch(err => {
-                iziToast.error({
-                    message: 'Sorry, there are no images matching your search query. Please try again!'
-                })
-                clearGallery();
-            }).finally(() => {
-                hideLoader();
-            })
 
+            createGallery(arrOfImage);
+        })
+        .catch(err => {
+            iziToast.error({
+                message: 'Sorry, there are no images matching your search query. Please try again!'
+            });
+        })
+        .finally(() => {
+            hideLoader();
+        });
 
     formEl.reset();
 });
